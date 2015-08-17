81a4f9b209f05c49069ad1cd116eddda7d49d80e
anibalsolon <anibal@intuitivus.com.br>
2015-08-17 10:44:07 -0300

    EIS agent filter

--- src/pucrs/agentcontest2015/env/EISArtifact.java
+++ src/pucrs/agentcontest2015/env/EISArtifact.java
@@ -5,10 +5,15 @@ import jason.NoValueException;
 import jason.asSyntax.Literal;
 
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.LinkedList;
+import java.util.List;
 import java.util.Map;
+import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.logging.Logger;
 
@@ -27,6 +32,7 @@ import eis.exceptions.PerceiveException;
 import eis.exceptions.RelationException;
 import eis.iilang.Action;
 import eis.iilang.EnvironmentState;
+import eis.iilang.Identifier;
 import eis.iilang.Parameter;
 import eis.iilang.Percept;
 
@@ -61,6 +67,14 @@ public class EISArtifact extends Artifact {
 		receiving = true;
 		execInternalOp("receiving");
 	}
+	
+	private static AgentId leader;
+	
+	@OPERATION
+	void register_leader() throws EntityException {
+		leader = getOpUserId();
+		System.out.println("Registering leader: " + leader.getAgentName());
+	}
 
 	@OPERATION
 	void register() throws EntityException {
@@ -70,7 +84,7 @@ public class EISArtifact extends Artifact {
 			ei.associateEntity(agent, agent);
 			agentToEntity.put(agent, agent);
 			agentIds.put(agent, getOpUserId());
-			System.out.println("Registering " + agent);
+			System.out.println("Registering: " + agent);
 		} catch (AgentException e) {
 			e.printStackTrace();
 		} catch (RelationException e) {
@@ -105,14 +119,17 @@ public class EISArtifact extends Artifact {
 	}
 
 	@INTERNAL_OPERATION
-	void receiving() {
+	void receiving() throws JasonException {
+		Set<Percept> leader_percepts = new HashSet<Percept>();
 		while (receiving) {
+			leader_percepts.clear();
 			for (String agent : ei.getAgents()) {
 				try {
 					Collection<Percept> percepts = ei.getAllPercepts(agent).get(agentToEntity.get(agent));
-					for (Percept percept : percepts) {
+					leader_percepts.addAll(agentise(agent, percepts));
+					for (Percept percept : filter(percepts)) {
 						String name = percept.getName();
-						Literal literal = Translator.perceptToLiteral(percept);						
+						Literal literal = Translator.perceptToLiteral(percept);
 						signal(agentIds.get(agent), name, (Object[]) literal.getTermsArray());
 					}
 				} catch (PerceiveException e) {
@@ -121,13 +138,85 @@ public class EISArtifact extends Artifact {
 					e.printStackTrace();
 				}
 			}
-			await_time(200);
+			for (Percept percept : leader_percepts) {
+				String name = percept.getName();
+				Literal literal = Translator.perceptToLiteral(percept);
+				signal(leader, name, (Object[]) literal.getTermsArray());
+			}
+			await_time(100);
 		}
 	}
 
 	@OPERATION
 	void stopReceiving() {
 		receiving = false;
+	}
+
+	static List<String> agentise = Arrays.asList(new String[]{
+		"charge",
+		"fPosition",
+		"inFacility",
+		"item",
+		"lastAction",
+		"lastActionParam",
+		"lastActionResult",
+		"lat",
+		"load",
+		"lon",
+		"requestAction",
+		"role",
+		"route",
+		"routeLength",
+	});
+
+	public static List<Percept> agentise( String agent, Collection<Percept> perceptions ){
+		List<Percept> list = new ArrayList<Percept>();
+		for(Percept perception : perceptions){
+			if(agentise.contains(perception.getName())){
+				LinkedList<Parameter> parameters = perception.getClonedParameters();
+				parameters.addFirst(new Identifier(agent));
+				perception = (Percept) perception.clone();
+				perception.setParameters(parameters);
+			}
+			list.add(perception);
+		}
+		return list;
+	}
+	
+	static List<String> agent_filter = Arrays.asList(new String[]{
+		"charge",
+		"entity",
+		"fPosition",
+		"inFacility",
+		"item",
+		"lastAction",
+		"lastActionParam",
+		"lastActionResult",
+		"lat",
+		"load",
+		"lon",
+		"requestAction",
+		"role",
+		"route",
+		"routeLength",
+		"team",
+		"timestamp",
+		
+		"shop",
+		"storage",
+		"workshop",
+		"chargingStation",
+		"dump",
+	});
+	
+	public static List<Percept> filter( Collection<Percept> perceptions ){
+		List<Percept> list = new ArrayList<Percept>();
+		for(Percept perception : perceptions){
+			if(agent_filter.contains(perception.getName())){
+				list.add(perception);
+			}
+		}
+		return list;
 	}
 
 }
