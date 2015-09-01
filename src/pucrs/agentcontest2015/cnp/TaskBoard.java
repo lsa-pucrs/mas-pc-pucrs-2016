83f6c1e9a30db6a84153f9121e914a1d56afdae9
Rafael C. Cardoso <rafael_caue@hotmail.com>
2015-09-01 15:02:22 -0300

    Adding Cartago artefacts for cnp.

---
+++ src/pucrs/agentcontest2015/cnp/TaskBoard.java
@@ -0,0 +1,33 @@
+// CArtAgO artifact code for project contract_net_protocol
+
+package pucrs.agentcontest2015.cnp;
+
+import cartago.*;
+
+public class TaskBoard extends Artifact {
+	
+	private int taskId;
+	
+	void init(){
+		System.out.println(" Artifact created!");
+		taskId = 0;
+	}
+	
+	@OPERATION void announce(String taskDescr, int duration, OpFeedbackParam<String> id){
+		taskId++;
+		try {
+			String artifactName = "cnp_board_"+taskId;
+			makeArtifact(artifactName, "pucrs.agentcontest2015.cnp.ContractNetBoard", new ArtifactConfig(taskDescr,duration));
+			defineObsProperty("task", taskDescr, artifactName);
+			id.set(artifactName);
+		} catch (Exception ex){
+			failed("announce_failed");
+		}
+	}
+	
+	@OPERATION void clear(String id){
+		String artifactName = "cnp_board_"+taskId;
+		this.removeObsPropertyByTemplate("task", null, artifactName);
+	}
+	
+}
